0a405eae71e4e1226c6d8832d8b2680b70e9cb0c
Justin Chin <jchingames@gmail.com>
2021-03-09 02:08:42 -0800

    partial work in progress, to get trail to stay if other keys are pressed when keys are lifted up

---
+++ Assets/ActivateTrail.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTrail : MonoBehaviour
+{
+    public GameObject trail;
+    public bool pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trail.SetActive(false);
+        Debug.Log("Turned Off");
+        pressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            trail.SetActive(true);
+            pressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            trail.SetActive(false);
+
+        }
+
+    }
+}
