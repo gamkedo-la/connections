623452aca6dd9b5314b4480980b85e3e10bdb100
Justin Chin <jchingames@gmail.com>
2022-09-02 18:47:52 -0700

    fixed player controller with new cam

--- Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -7,10 +7,11 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 0.5f;
+    public Transform groundCheck;
+    public float speed = 6f;
     public float speedDampen = 0.8f;
     private Vector3 drift = Vector3.zero;
-    public float turnSmoothTime = 0.1f;
+    public float turnSmoothTime = 0.2f;
     float turnSmoothVelocity;
 
     void FixedUpdate()
@@ -28,6 +29,7 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
             drift += (moveDir.normalized * speed);
         }
     }
